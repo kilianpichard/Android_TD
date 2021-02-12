47bc259b5b3e2c5fa6d548d2850cb9f2e5b279de
Kilian Pichard <kil.pichard@icloud.com>
2021-02-12 13:38:01 +0100

    AjoutMagasin working with toast

--- app/src/main/java/com/example/td1/AjoutMagasin.java
+++ app/src/main/java/com/example/td1/AjoutMagasin.java
@@ -2,7 +2,12 @@ package com.example.td1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AjoutMagasin extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class AjoutMagasin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajout_magasin);
+    }
+    public void addMagasin(View view) {
+      EditText champ = (EditText) findViewById(R.id.editMagasin);
+      if (champ.getText().toString().length() != 0) {
+        Intent retour = new Intent();
+        retour.putExtra("nomMagasin", champ.getText().toString());
+        setResult(1, retour);
+        finish();
+      }
     }
 }
--- app/src/main/java/com/example/td1/ListeMagasins.java
+++ app/src/main/java/com/example/td1/ListeMagasins.java
@@ -9,8 +9,10 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Gravity;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class ListeMagasins extends AppCompatActivity {
 
@@ -20,11 +22,36 @@ public class ListeMagasins extends AppCompatActivity {
         setContentView(R.layout.activity_liste_magasins);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Boutton de retour vers l'activité parente
 
     }
 
     public void onClickAjout(View view) {
-        startActivity(new Intent(this, AjoutMagasin.class));
+        Intent ajoutActivity = new Intent(this, AjoutMagasin.class);
+        startActivityForResult(ajoutActivity,1);
+        onActivityResult(1,1,ajoutActivity); //Request code de 1 pour créer, 2 pour modifier
     }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    super.onActivityResult(requestCode, resultCode, intent);
+    try{
+      String nomMagasin = intent.getStringExtra("nomMagasin");
+      if(!(nomMagasin == null)){
+        Toast toast;
+        if(requestCode == 1) {
+          toast = Toast.makeText(this, "Vous avez ajouter :  " + nomMagasin, Toast.LENGTH_SHORT);
+        }else {
+          toast = Toast.makeText(this, "Vous avez modifier :  " + nomMagasin, Toast.LENGTH_SHORT);
+        }
+        toast.setGravity(Gravity.CENTER, 0, -100);
+        toast.show();
+      }
+    }catch (NullPointerException e){
+      e.printStackTrace();
+    }
+
+
+
+  }
 }
