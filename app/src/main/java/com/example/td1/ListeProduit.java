08c8feb5f606e11b4fbcbb73196382491b3ea8e3
Kilian Pichard <kil.pichard@icloud.com>
2021-02-12 15:08:32 +0100

    Add list of some products (int prix -> double prix)

--- app/src/main/java/com/example/td1/ListeProduit.java
+++ app/src/main/java/com/example/td1/ListeProduit.java
@@ -10,6 +10,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ListeProduit extends AppCompatActivity {
 
   @Override
@@ -19,5 +22,13 @@ public class ListeProduit extends AppCompatActivity {
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Boutton de retour vers l'activité parente
+
+    final List<Produit> produits = Arrays.asList(
+      new Produit("Boissons","Pack 6 Bière",12.99, 0,"Rayon Alcool"),
+      new Produit("Condiments","Huile d'olive 50cl",5.79, 10,"Rayon Huile"),
+      new Produit("Soins dentaire","Dentifrice Colgate",3.59, 20,"Rayon Hygiène"),
+      new Produit("Pommes","Pommes Golden (1kg)",2.38, 0,"Rayon Fruits & Légumes")
+    );
+
   }
 }
--- app/src/main/java/com/example/td1/Produit.java
+++ app/src/main/java/com/example/td1/Produit.java
@@ -3,16 +3,17 @@ package com.example.td1;
 public class Produit {
   public String categorie;
   public String nom;
-  public int prix;
-  public int promotion;
+  public double prix;
+  public int promotion; // en %
   public String emplacement;
 
-  public Produit(String categorie, String nom, int prix, int promotion, String emplacement){
+  public Produit(String categorie, String nom, double prix, int promotion, String emplacement){
     this.categorie = categorie;
     this.nom = nom;
     this.prix = prix;
     this.emplacement = emplacement;
     this.categorie = categorie;
+    this.promotion = promotion;
   }
 
 }
