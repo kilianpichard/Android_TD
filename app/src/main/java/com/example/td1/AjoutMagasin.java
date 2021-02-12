131be69ab834b2c3fa8322ae2944a22a705b80bb
Kilian Pichard <kil.pichard@icloud.com>
2021-02-12 13:43:27 +0100

    Add back button AjoutMagasin + reformat code

--- app/src/main/java/com/example/td1/AjoutMagasin.java
+++ app/src/main/java/com/example/td1/AjoutMagasin.java
@@ -1,28 +1,31 @@
 package com.example.td1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class AjoutMagasin extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ajout_magasin);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_ajout_magasin);
+    Toolbar toolbar = findViewById(R.id.toolbar2);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Boutton de retour vers l'activité parente
+  }
+
+  public void addMagasin(View view) {
+    EditText champ = (EditText) findViewById(R.id.editMagasin);
+    if (champ.getText().toString().length() != 0) {
+      Intent retour = new Intent();
+      retour.putExtra("nomMagasin", champ.getText().toString());
+      setResult(1, retour);
+      finish();
     }
-    public void addMagasin(View view) {
-      EditText champ = (EditText) findViewById(R.id.editMagasin);
-      if (champ.getText().toString().length() != 0) {
-        Intent retour = new Intent();
-        retour.putExtra("nomMagasin", champ.getText().toString());
-        setResult(1, retour);
-        finish();
-      }
-    }
+  }
 }
--- app/src/main/java/com/example/td1/ListeMagasins.java
+++ app/src/main/java/com/example/td1/ListeMagasins.java
@@ -2,55 +2,48 @@ package com.example.td1;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.Gravity;
-import android.view.View;
-import android.content.Intent;
-import android.widget.Toast;
-
 public class ListeMagasins extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_liste_magasins);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Boutton de retour vers l'activité parente
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_liste_magasins);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Boutton de retour vers l'activité parente
+  }
 
-    }
-
-    public void onClickAjout(View view) {
-        Intent ajoutActivity = new Intent(this, AjoutMagasin.class);
-        startActivityForResult(ajoutActivity,1);
-        onActivityResult(1,1,ajoutActivity); //Request code de 1 pour créer, 2 pour modifier
-    }
+  public void onClickAjout(View view) {
+    Intent ajoutActivity = new Intent(this, AjoutMagasin.class);
+    startActivityForResult(ajoutActivity, 1);
+    onActivityResult(1, 1, ajoutActivity); //Request code de 1 pour créer, 2 pour modifier
+  }
 
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent intent) {
     super.onActivityResult(requestCode, resultCode, intent);
-    try{
+    try {
       String nomMagasin = intent.getStringExtra("nomMagasin");
-      if(!(nomMagasin == null)){
+      if (!(nomMagasin == null)) {
         Toast toast;
-        if(requestCode == 1) {
+        if (requestCode == 1) {
           toast = Toast.makeText(this, "Vous avez ajouter :  " + nomMagasin, Toast.LENGTH_SHORT);
-        }else {
+        } else {
           toast = Toast.makeText(this, "Vous avez modifier :  " + nomMagasin, Toast.LENGTH_SHORT);
         }
         toast.setGravity(Gravity.CENTER, 0, -100);
         toast.show();
       }
-    }catch (NullPointerException e){
+    } catch (NullPointerException e) {
       e.printStackTrace();
     }
-
 
 
   }
