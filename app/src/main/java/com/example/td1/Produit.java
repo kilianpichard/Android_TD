4f1d1e81b9330a2ec1cea87806128ec058efc2aa
Kilian Pichard <kil.pichard@icloud.com>
2021-02-12 14:39:14 +0100

    Add Produit's model

---
+++ app/src/main/java/com/example/td1/Produit.java
@@ -0,0 +1,18 @@
+package com.example.td1;
+
+public class Produit {
+  public String categorie;
+  public String nom;
+  public int prix;
+  public int promotion;
+  public String emplacement;
+
+  public Produit(String categorie, String nom, int prix, int promotion, String emplacement){
+    this.categorie = categorie;
+    this.nom = nom;
+    this.prix = prix;
+    this.emplacement = emplacement;
+    this.categorie = categorie;
+  }
+
+}
