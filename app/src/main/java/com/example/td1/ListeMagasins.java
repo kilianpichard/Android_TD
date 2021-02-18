2f0ba8da1802ddf721266814ab1ea8c33509cd8b
Kilian Pichard <kil.pichard@icloud.com>
2021-02-18 14:33:40 +0100

    Fix Bugs, Add Magasins (still Activity)

--- app/src/main/java/com/example/td1/ListeMagasins.java
+++ app/src/main/java/com/example/td1/ListeMagasins.java
@@ -6,6 +6,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -13,33 +14,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class ListeMagasins extends Fragment {
 
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.activity_liste_magasins, container, false);
-    //  final Intent intent = new Intent(ListeMagasins, AjoutMagasin.class);
+    final Intent intent = new Intent(getActivity(), AjoutMagasin.class);
     final ListView listeMagasins = (ListView) view.findViewById(R.id.listMagasins);
-  /*  listeMagasins.setOnItemClickListener((parent, view1, position, id) -> {
+    listeMagasins.setOnItemClickListener((parent, view1, position, id) -> {
       intent.putExtra("nomMagasin", listeMagasins.getItemAtPosition(position).toString());
       intent.putExtra("text", "Changer " + listeMagasins.getItemAtPosition(position).toString() + " en :");
       startActivityForResult(intent, 1);
       onActivityResult(2, 1, intent);
-    });*/
+    });
+    FloatingActionButton add =  view.findViewById(R.id.add);
+    add.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        intent.putExtra("nomMagasin","");
+        intent.putExtra("text", "Ajouter un Magasin");
+        startActivityForResult(intent, 1);
+        onActivityResult(2, 1, intent);
+      }
+    });
     return view;
   }
 
-    public void onClickAjout (View view){
-      Intent ajoutActivity = new Intent(getActivity(), AjoutMagasin.class);
-      ajoutActivity.putExtra("nomMagasin", "");
-      ajoutActivity.putExtra("text", "Ajouter un Magasin");
-      startActivityForResult(ajoutActivity, 1);
-      onActivityResult(1, 1, ajoutActivity);
-    }
-
-  /*public void onActivityResult(int requestCode, int resultCode, Bundle intent) {
+  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
     super.onActivityResult(requestCode, resultCode, intent);
     try {
-      String nomMagasin = bundle.getStringExtra("nomMagasin");
+      String nomMagasin = intent.getStringExtra("nomMagasin");
       if (!(nomMagasin == null)) {
         Toast toast;
         if (requestCode == 1) {
@@ -53,7 +58,5 @@ public class ListeMagasins extends Fragment {
     } catch (NullPointerException e) {
       e.printStackTrace();
     }
-
-
-  }*/
+  }
 }
