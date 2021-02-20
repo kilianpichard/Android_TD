9c154e1b1cc3b1f38d6bae2be571961e410ee1e7
Laurent Jeanpierre <laurent.jeanpierre@unicaen.fr>
2021-02-20 12:12:48 +0100

    Très bon travail pour le TP4. Il manque le fragment initial et la personalisation du menu, mais l'essentiel y est. B

--- app/src/main/java/com/example/td1/ListeProduit.java
+++ app/src/main/java/com/example/td1/ListeProduit.java
@@ -24,7 +24,6 @@ import java.util.List;
 public class ListeProduit extends Fragment {
 
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
-
                            Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.activity_liste_produits, container, false);
     final List<Produit> produits = Arrays.asList(
