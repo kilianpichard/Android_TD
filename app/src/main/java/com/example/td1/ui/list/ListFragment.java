d5e1475e6b24dc9d7a2d404988cb94a871ee1b10
Clement Hellot <clement.hellot.etu@gmail.com>
2021-03-24 10:54:12 +0100

    fix merge fail

--- app/src/main/java/com/example/td1/ui/list/ListFragment.java
+++ app/src/main/java/com/example/td1/ui/list/ListFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.td1.R;
@@ -16,8 +19,10 @@ public class ListFragment extends Fragment {
     private ListViewModel listViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        listViewModel =
+                new ViewModelProvider(this).get(ListViewModel.class);
         View root = inflater.inflate(R.layout.fragment_list, container, false);
         return root;
     }
-
 }
