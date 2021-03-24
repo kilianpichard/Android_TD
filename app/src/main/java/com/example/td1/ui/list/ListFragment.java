a3134cdff9c0133364336c9699b2a2e63600a577
Kilian <kil.pichard@icloud.com>
2021-03-24 10:48:46 +0100

    Clean up import

--- app/src/main/java/com/example/td1/ui/list/ListFragment.java
+++ app/src/main/java/com/example/td1/ui/list/ListFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.td1.R;
@@ -18,11 +15,10 @@ public class ListFragment extends Fragment {
 
     private ListViewModel listViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        listViewModel =
-                new ViewModelProvider(this).get(ListViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        listViewModel = new ViewModelProvider(this).get(ListViewModel.class);
         View root = inflater.inflate(R.layout.fragment_list, container, false);
         return root;
     }
+
 }
--- app/src/main/java/com/example/td1/ui/list/ListViewModel.java
+++ app/src/main/java/com/example/td1/ui/list/ListViewModel.java
@@ -1,8 +1,13 @@
 package com.example.td1.ui.list;
 
+import android.content.Intent;
+import android.view.View;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.td1.ui.slideshow.SlideshowFragment;
 
 public class ListViewModel extends ViewModel {
 
