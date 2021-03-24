6277fbbc1e4efcd59acb70d044abe8955c92d2b4
Clement Hellot <clement.hellot.etu@gmail.com>
2021-03-24 12:33:35 +0100

    Nouvelle note spinner

--- app/src/main/java/com/example/td1/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/td1/ui/slideshow/SlideshowFragment.java
@@ -4,12 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.td1.R;
@@ -23,6 +22,8 @@ public class SlideshowFragment extends Fragment {
         slideshowViewModel =
                 new ViewModelProvider(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+
+
 
         return root;
     }
